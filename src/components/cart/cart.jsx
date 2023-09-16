dbfd9aae8ef568946efd786dba03aaaaccf53bd4
rejaul921 <rejaulkarim921@gmail.com>
2023-09-16 15:31:40 +0400

    loaded json data by using useEffect and set them in useState by setCourses also designed the initial UI

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 function Cart() {
     return (
         <>
-        
+        <div className='mb-4 text-[#2F80ED]'>
+            Credit Hours Remaining <span id='credit-hrs'></span> hrs  
+        </div>
+         <hr />
+
+         <div>
+            <h3>Courses Name</h3>
+            <ol className='list-decimal text-sm px-4'>
+            {
+            
+         }
+            </ol>
+         </div>
+         <hr />
+         <div className='my-4 text-base font-semibold'><p>Total Credit Hours :<span></span></p></div>
+         <hr />
+         <div><p>Total Price :<span></span>$ USD</p></div>
         </>
     )
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Cart from '../cart/cart'
 
 function Home() {
+    const [courses, setCourses]=useState([]);
+
+useEffect(()=>{
+    fetch('courses.json')
+    .then(res=>res.json())
+    .then(data=>setCourses(data))
+},[]);
     return (
         <>
-        <div className='flex w-4/5 mx-auto'>
+        <div className='flex w-11/12 gap-2 mx-auto'>
+        <div className='w-2/3 grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center text-2xl font-bold mx-4'> 
+            {
+                courses.map(course=> <div key={course.id} className='bg-slate-100 shadow-xl p-3 rounded-xl mb-8 grid'>
+                <div>
+                <div><img src={course.img} alt="" /></div>
+                <h3 className='text-xl font-bold text-center'>{course.title}</h3>
+                <p className='text-sm font-normal'>{course.description}</p>
+                <div className='flex justify-between items-center my-4 px-3'>
+                    <div><p className='text-sm'>price:<span>{course.price}</span>$</p></div>
+                    <div className='flex justify-between items-center'>
+                        <div className='ml-2 w-4 h-4'><img src="/Frame.png" alt="" /></div>
+                        <div><p className='text-sm'>Credit:<span id='creditTime'>{course.reading_time}</span>hrs</p></div>
+                    </div>
+                </div>
+                <div className='flex-grow'></div>
+                </div>
+                <div className='mb-0 w-full flex mx-auto justify-center items-end'><button className='w-full flex mx-auto justify-center items-end text-white text-center bg-[#2F80ED]'>Select</button></div>
+            </div>)
+            }
+            </div>
+            <div className='w-1/4 text-center text-xl font-bold bg-slate-100 shadow-xl p-3 rounded-xl'>
+                <Cart>
+
+                </Cart>
+            </div>
 
         </div>
         </>

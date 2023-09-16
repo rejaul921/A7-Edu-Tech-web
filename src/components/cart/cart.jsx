69631c7561f91f90a0304b08b7bf86fb3b3e150f
rejaul921 <rejaulkarim921@gmail.com>
2023-09-16 16:10:12 +0400

    functionality added for select course button

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Cart() {
+function Cart({selectedCourses,totalMoney,totalCostHr}) {
     return (
         <>
         <div className='mb-4 text-[#2F80ED]'>
-            Credit Hours Remaining <span id='credit-hrs'></span> hrs  
+            Credit Hours Remaining <span>{20-totalCostHr}</span> hrs  
         </div>
          <hr />
 
@@ -12,14 +12,16 @@ function Cart() {
             <h3>Courses Name</h3>
             <ol className='list-decimal text-sm px-4'>
             {
-            
+             selectedCourses.map(course=>(
+             <li key={course.id}>{course.title}</li>
+            ))
          }
             </ol>
          </div>
          <hr />
-         <div className='my-4 text-base font-semibold'><p>Total Credit Hours :<span></span></p></div>
+         <div className='my-4 text-base font-semibold'><p>Total Credit Hours : <span>{totalCostHr}</span></p></div>
          <hr />
-         <div><p>Total Price :<span></span>$ USD</p></div>
+         <div><p>Total Price : <span>{totalMoney}</span>$ USD</p></div>
         </>
     )
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,12 +3,39 @@ import Cart from '../cart/cart'
 
 function Home() {
     const [courses, setCourses]=useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+    const [totalMoney, setTotalMoney] = useState(0);
+    const [totalCostHr, setTotalCostHr] = useState(0);
 
 useEffect(()=>{
     fetch('courses.json')
     .then(res=>res.json())
     .then(data=>setCourses(data))
 },[]);
+
+const selectCourse=(course)=>{
+    const isSelected=selectedCourses.find((item)=>item.id==course.id);
+    let hours=course.reading_time
+    let money=course.price
+    if(isSelected){
+        alert("You have already added the Course")
+    }
+    else{
+        selectedCourses.forEach((item)=>{
+            hours=hours+item.reading_time
+            money=money+item.price
+        });
+        if(hours>20){
+            alert("Sorry! You've run out of Credit hours");
+        }
+        else{
+            setTotalMoney(money)
+            setTotalCostHr(hours)
+            setSelectedCourses([...selectedCourses,course])
+        }
+    }
+
+}
     return (
         <>
         <div className='flex w-11/12 gap-2 mx-auto'>
@@ -28,14 +55,16 @@ useEffect(()=>{
                 </div>
                 <div className='flex-grow'></div>
                 </div>
-                <div className='mb-0 w-full flex mx-auto justify-center items-end'><button className='w-full flex mx-auto justify-center items-end text-white text-center bg-[#2F80ED]'>Select</button></div>
+                <div onClick={()=>selectCourse(course)} className='mb-0 w-full flex mx-auto justify-center items-end'><button className='w-full flex mx-auto justify-center items-end text-white text-center bg-[#2F80ED]'>Select</button></div>
             </div>)
             }
             </div>
             <div className='w-1/4 text-center text-xl font-bold bg-slate-100 shadow-xl p-3 rounded-xl'>
-                <Cart>
-
-                </Cart>
+                <Cart
+                    selectedCourses={selectedCourses}
+                    totalMoney={totalMoney}
+                    totalCostHr={totalCostHr}
+                ></Cart>
             </div>
 
         </div>

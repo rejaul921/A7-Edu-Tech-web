ec81d33aebcb4f907710caa00e488dbc6655226c
rejaul921 <rejaulkarim921@gmail.com>
2023-09-16 14:20:37 +0400

    created home and cart componenetsfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
+import Home from './components/home/home'
 
 function App() {
 
 
   return (
     <>
-
-
-
-
+    <h3 className='text-2xl text-center font-semibold my-8'>Course Registration</h3>
+    <Home></Home>
     </>
   )
 }
---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+        <div className='flex w-4/5 mx-auto'>
+
+        </div>
+        </>
+    )
+}
+
+export default Home
